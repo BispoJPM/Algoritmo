72f80319662cd747add8be964cce86e0c987a26d
João Pedro <142633184+BispoJPM@users.noreply.github.com>
2023-09-06 17:43:01 -0300

    Update Algoritmo33.py

--- Algoritmo33.py
+++ Algoritmo33.py
@@ -1,6 +1,6 @@
 
-num1 = input("\nEntre com o primeiro número:9")
-num2 = input("\nEntre com o segundo número:7")
+num1 = input("\nEntre com o primeiro número: ")
+num2 = input("\nEntre com o segundo número: ")
 print("\nNúmero 1:", num1)
 print("Número 2:", num2)
 print("\n")
