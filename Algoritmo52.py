438a2f37d32786bd993a9664b30da14ac75fe54e
7326564Jp <marcondesjpb@gmail.com>
2023-09-07 22:02:55 -0300

    lol

---
+++ Algoritmo52.py
@@ -0,0 +1,10 @@
+lado=float(input("\nDigite o lado do quadrado: "))
+pm=4*lado
+area=lado**2
+diagonal=lado**1/2
+print("\nPerimetro: ", pm)
+print("\nArea: ", area)
+print("\nDiagonal: ", diagonal, "\n")
+
+
+
---
+++ Algoritmo53.py
@@ -0,0 +1,5 @@
+a=float(input("\nEntre com a base: "))
+b=float(input("\nEntre com a altura: "))
+c=float(input("\nEntre com a profundidade: "))
+diagonal=(a**2+b**2+c**2)**1/2
+print("\nDiagonal: ", diagonal, "\n")
